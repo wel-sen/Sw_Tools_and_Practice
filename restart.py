102c281a38ecc8b62306cfe6e4397324278370b9
yohanne <yohannes@2gmail.com>
2021-07-09 23:24:50 +0300

    editted

--- restart.py
+++ restart.py
@@ -7,6 +7,4 @@ restart = input("Do want to play Again?(y/n)")
 
     if restart == "y" or restart == "Y":  
         for key in board_keys:
-            theBoard[key] = " "
-
-        game()    
+            theBoard[k
